734506bbc0418b963987dd2f5a0efcea17e9a442
Michael Santos <michael.santos@gmail.com>
2026-05-19 08:43:12 -0400

    test: supervises: stdin

--- pkg/supervises/supervises_test.go
+++ pkg/supervises/supervises_test.go
@@ -1,10 +1,14 @@
 package supervises_test
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"sync"
 	"testing"
 	"time"
 
@@ -47,14 +51,14 @@ func TestOpt_Supervise(t *testing.T) {
 			return
 		}
 
-		if ee.Error() != "Exited with status 137: signal: killed" {
-			t.Errorf("supervisor error: %s", ee.Error())
-			return
-		}
-
-		if ee.ExitCode != 137 {
-			t.Errorf("unexpected exit status: %d: %s", ee.ExitCode, ee.Error())
-			return
+		if ee != nil {
+			switch ee.ExitCode {
+			// The exit code returned will depend on which call was interruped by the cancelled context.
+			case 126, 128, 137:
+			default:
+				t.Errorf("unexpected exit status: %d: %s", ee.ExitCode, ee.Error())
+				return
+			}
 		}
 
 		return
@@ -108,6 +112,85 @@ func TestOpt_Supervise_retry(t *testing.T) {
 	}
 
 	t.Error("supervisor exited")
+}
+
+type Stdout struct {
+	mu  sync.Mutex
+	buf bytes.Buffer
+}
+
+func (s *Stdout) Write(p []byte) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.buf.Write(p)
+}
+
+func (s *Stdout) Bytes() []byte {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return append([]byte(nil), s.buf.Bytes()...)
+}
+
+var ErrExitSuccess = errors.New("exited")
+
+func TestOpt_Supervise_stdin(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	//supervised := []string{"@read line; echo $line", "@read line; echo $line", "@read line; echo $line"}
+	supervised := []string{"cat", "cat", "cat"}
+	var wg sync.WaitGroup
+	wg.Add(len(supervised))
+
+	s := supervises.New(ctx,
+		supervises.WithStdin(io.NopCloser(bytes.NewReader([]byte("testing 123\n")))),
+		supervises.WithRetry(
+			func(_ *supervises.Cmd, _ *supervises.ExitError) *supervises.ExitError {
+				wg.Done()
+				wg.Wait()
+				return &supervises.ExitError{
+					Cmd: &exec.Cmd{
+						Path: "cat",
+						Args: []string{"cat"},
+					},
+					Err:      nil,
+					ExitCode: 0,
+				}
+			},
+		),
+	)
+
+	cmds, err := s.Cmd(supervised...)
+	if err != nil {
+		t.Errorf("invalid command: %v", err)
+		return
+	}
+
+	stdout := &Stdout{}
+
+	for _, cmd := range cmds {
+		cmd.Stdout = stdout
+	}
+
+	if err := s.Supervise(cmds...); err != nil {
+		var ee *supervises.ExitError
+
+		if !errors.As(err, &ee) {
+			t.Errorf("%v", err)
+			return
+		}
+
+		if ee.ExitCode != 0 {
+			t.Errorf("unexpected exit status: %d: %s", ee.ExitCode, ee.Error())
+			return
+		}
+	}
+
+	buf := stdout.Bytes()
+	if !bytes.Equal(buf, []byte("testing 123\ntesting 123\ntesting 123\n")) {
+		t.Errorf("unexpected output: %s", buf)
+		return
+	}
 }
 
 func ExampleOpt_Supervise() {
