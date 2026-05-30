d6fec64f77b81f147434c7da64f55d831b23e2b9
Michael Santos <michael.santos@gmail.com>
2026-05-30 09:14:28 -0400

    example: run with signal handling

--- pkg/supervises/example_test.go
+++ pkg/supervises/example_test.go
@@ -33,3 +33,29 @@ func ExampleSupervisor_Run() {
 
 	// Output: test123
 }
+
+func ExampleSupervisor_Run_signal_handling() {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	cmds, err := supervises.Parse("@echo test123; exec sleep 10", "cat")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	sv := supervises.New(ctx, cmds)
+
+	supervises.ForwardSignals(ctx, sv, supervises.DefaultSignals...)
+
+	if err = sv.Run(); err != nil {
+		var ee *supervises.ExitError
+
+		if !errors.As(err, &ee) {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return
+		}
+	}
+
+	// Output: test123
+}
